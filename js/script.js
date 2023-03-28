cebd85257dac15f923f8e1a18ca2825666fa0c61
Jonathan Varela <jonvarela4@gmail.com>
2023-03-28 14:38:49 -0400

    Added movie details

--- js/script.js
+++ js/script.js
@@ -70,6 +70,72 @@ async function displayPopularShows() {
   })
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div')
+
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+    ?`<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+  }
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: XX/XX/XXXX</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+      atque molestiae error debitis provident dolore hic odit, impedit
+      sint, voluptatum consectetur assumenda expedita perferendis
+      obcaecati veritatis voluptatibus. Voluptatum repellat suscipit,
+      quae molestiae cupiditate modi libero dolorem commodi obcaecati!
+      Ratione quia corporis recusandae delectus perspiciatis consequatur
+      ipsam. Cumque omnis ad recusandae.
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      <li>Genre 1</li>
+      <li>Genre 2</li>
+      <li>Genre 3</li>
+    </ul>
+    <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $1,000,000</li>
+    <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+    <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+    <li><span class="text-secondary">Status:</span> Released</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">Company 1, Company 2, Company 3</div>
+</div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = 'ff905e9433e752bfbd5c6df919234e63';
@@ -114,7 +180,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details')
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
